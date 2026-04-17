f560fcecd10ef46f8ec101ff2453d5565681d7a2
danu040 <danusha-pe23040@stu.kln.ac.lk>
2026-04-18 00:09:59 +0530

    Create StaffMember.java

---
+++ StaffMember.java
@@ -0,0 +1,107 @@
+abstract class StaffMember {
+    private String fullName;
+    private final String staffId;
+    protected String department;
+
+   public StaffMember(String fullName, String staffId, String department) 
+  {
+        this.fullName = fullName;
+        this.staffId = staffId;
+        this.department = department;
+    }
+
+    public String getFullName()
+  {
+        return fullName;
+    }
+
+    public String getStaffId()
+  {
+        return staffId;
+    }
+
+    public String getDepartment() 
+  {
+        return department;
+    }
+
+    public final void displayBasicDetails()
+  {
+        System.out.println("Name: " + fullName);
+        System.out.println("Staff ID: " + staffId);
+        System.out.println("Department: " + department);
+    }
+
+    public abstract double calculateMonthlyPayment();
+}
+
+
+
+abstract class StaffMember {
+    private String fullName;
+    private final String staffId;
+    protected String department;
+
+
+private static int staffCount = 0;
+
+public StaffMember(String fullName, String staffId, String department)
+  {
+    this.fullName = fullName;
+    this.staffId = staffId;
+    this.department = department;
+    staffCount++; // increase count
+}
+
+public static void showSystemName() 
+  {
+    System.out.println("Campus Staff Payment System");
+}
+
+public static int getStaffCount() 
+  {
+    return staffCount;
+    }
+}
+
+
+
+abstract class StaffMember
+  {
+    private String fullName;
+    private final String staffId;
+    protected String department;
+
+
+private static int staffCount = 0;
+
+public static void showSystemName() 
+    {
+    System.out.println("Campus Staff Payment System");
+}
+
+public static int getStaffCount() 
+    {
+    return staffCount;
+}
+
+public StaffMember(String fullName, String staffId, String department) 
+    {
+    this.fullName = fullName;
+    this.staffId = staffId;
+    this.department = department;
+    staffCount++; 
+}
+
+public void changeDepartment(String newDepartment)
+    {
+    if (newDepartment != null && !newDepartment.isEmpty()) 
+    {
+        this.department = newDepartment;
+    }
+}
+
+public final void showCommonNotice() {
+    System.out.println("All staff must follow university policies.");
+    }
+}
